7f2943fd8377f40a921a66ccaf32fa14d82770f9
tdamaville <theo.damaville@atinternet.com>
2017-05-17 16:50:29 +0200

    btn add

---
+++ src/Add/Add.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Add.css';
+
+export const Add = (props) => (
+  <div className="watcher-add">
+    <div className="watcher-add-cross">
+      <svg className="watcher-add-cross-svg" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet"><g><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" ></path></g></svg>
+    </div>
+  </div>
+);
