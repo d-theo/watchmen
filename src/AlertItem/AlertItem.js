9f381102caf4c6d21eb4754e94d812658d1793ba
BenDz <bdiolez@gmail.com>
2017-05-19 10:25:02 +0200

    :art: Alert deletion animation

--- src/AlertItem/AlertItem.js
+++ src/AlertItem/AlertItem.js
@@ -3,24 +3,29 @@ import './AlertItem.css';
 import {ProgressBar} from '../ProgressBar/ProgressBar.js';
 import {ComparisonBar} from '../ComparisonBar/ComparisonBar.js';
 import axios from 'axios';
+import {poller} from '../Poller.js';
 
 export class AlertItem extends Component {
+
+  constructor() {
+    super();
+    this.state = { alertDeleted : false };
+  }
 
   deleteAlert() {
     const alertId = this.props.alert.id;
     console.log(alertId);
+    this.setState({alertDeleted : true});
     axios.delete(`https://fnuhd0lu6a.execute-api.eu-west-1.amazonaws.com/dev/alerts/${alertId}`)
-      /*.then(response => {
+      .then(response => {
+        poller.fetch();
         console.log(response);
-        this.setState({'alertItems': response.data })
-      }).catch(e => {
-        console.log(e)
-        this.setState({'alertItems': [{"id":"9aeb9f07-d964-4904-a067-a35ddde1e8df","name":"MOCKMOCKMOCJK"},{"last-value":50,"user":{"name":"jpiquet@xiti.com","id":258945},"period":"R:{D:0}","site":{"name":"Hit-Parade","id":1},"name":"Alerte 1","state":"ok","value":100,"notifications":{"ifttt":[{}],"mails":["jpiquet@xiti.com"],"slack":[{}]},"direction":"up","description":"alerte alerte alerte !","id":"alerte1","last-value-date":"timestamp","metric":{"name":"Loads","id":"m_page_loads"},"type":"absolute"}] })
-    })*/
+      })
   }
 
   render() {
     let progressBarComp = '';
+    let deleted = this.state.alertDeleted ? 'w-alert-deleted' : '';
 
     if(this.props.alert.type == 'relative') {
       progressBarComp = <ProgressBar alert={this.props.alert} />;
@@ -32,14 +37,14 @@ export class AlertItem extends Component {
     let date = new Date(this.props.alert.lastValueDate);
 
     return (
-      <div className="w-alert-item">
+      <div className={`w-alert-item ${deleted}`}>
         <div className="w-alert-item-header">
-          <h2>{this.props.alert.label}</h2>
+          <h2>{this.props.alert.label} <br /><span className="w-alert-site">{this.props.alert.site.label}</span></h2>
           <a className="w-delete-alert" onClick={() => this.deleteAlert()}><i className="icon-minus w-delete-alert-icon" aria-hidden="true"></i></a>
         </div>
         <h4>{this.props.alert.description}</h4>
         {progressBarComp}
-        <small>Checked on the {date.getFullYear()}/{date.getMonth()+1}/{date.getDate()}, {date.getHours()}h{date.getMinutes()}m{date.getSeconds()}s: {this.props.alert.lastValue}/{this.props.alert.threshold}</small>
+        <small>Checked on the {date.getFullYear()}/{date.getMonth()+1}/{date.getDate()}, {date.getHours()}h{date.getMinutes()}m{date.getSeconds()}s: {this.props.alert.lastValue}/{this.props.alert.threshold} {this.props.alert.metric.label}</small>
         
       </div>
     );
