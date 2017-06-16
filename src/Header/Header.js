ac62f95cc44708fba93528452d90839becfe414a
Erwan Lumbroso <erwan.lumbroso@gmail.com>
2017-06-16 15:02:32 +0200

    Header + correction font

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,6 +7,6 @@ export const Header = (props) => (
   <div className="watcher-header">
     <img className="watcher-logo" src="logo_W.png" onClick={browserHistory.goBack}/>
     <h1 className="watcher-header-title">Watcher</h1>
-    <i className="icon-menu" aria-hidden="true"></i>
+    <i className="watcher-ico-menu icon-menu" aria-hidden="true"></i>
   </div>
 );
