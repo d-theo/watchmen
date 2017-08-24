4f17a7fb1a06d30405c3e5209e396de1fb48daca
BenDz <bdiolez@gmail.com>
2017-08-24 09:49:25 +0200

    Start poller only when authed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ let checkAuth = (nextState, replace) => {
 class App extends Component {
   constructor(){
     super()
-    poller.start()
+    if(authSvc.isAuthed()) poller.start();
   }
 
   render() {
